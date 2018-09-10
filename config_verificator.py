a63f01978528725c5770ce39a2d3c00afdefe5a3
Pasha <pasha@evermarkets.com>
2018-09-10 15:47:14 -0400

    added class to verify the given config

---
+++ config_verificator.py
@@ -0,0 +1,9 @@
+from spletnya.exceptions import Misconfiguration
+
+class ConfigVerificator():
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def config_is_valid(self):
+        #TODO add config verification
+        pass
