f88119343c7a9d9de56f8e3be55b1f395fcf9ccf
Pasha <pasha@evermarkets.com>
2018-09-11 17:58:23 -0400

    added messages file

---
+++ molva/messages.py
@@ -0,0 +1,24 @@
+class NodesRequest():
+    def __init__(self):
+        self.node_id = ""
+
+class StateUpdate():
+    def __init__(self):
+        self.node_id = ""
+        self.timestamp = 0
+        self.new_state = ""
+
+class NodesInfo():
+    def __init__(self):
+        self.node_ids_to_ports = {}
+        self.node_ids_to_states = {}
+
+class NewActiveNode():
+    def __init__(self):
+        self.node_id = ""
+        self.timestamp = 0
+        self.state = ""
+
+class NodeExit():
+    def __init__(self):
+        self.node_id = ""
